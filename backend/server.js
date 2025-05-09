9fd58afb517e62d31026d5d4b9e7651417e73509
Vihanga <vihangahw@gmail.com>
2025-05-09 12:26:26 +0530

    add blog routes

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,13 @@ const cors = require('cors');
 const app = express();
 const serverPort = 4000;
 const authRoutes = require('./routes/authRoutes');
+const blogRoutes = require('./routes/blogRoutes');
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
 app.use('/auth', authRoutes);
+app.use('/blogs', blogRoutes);
 
 app.get('/', (request, response) => {
   response.send('Welcome to TravelTales!');
