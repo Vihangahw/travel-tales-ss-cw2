860c3eaebe499c5c9d1ee18d4722fb45883e92da
Vihanga <vihangahw@gmail.com>
2025-05-10 15:41:37 +0530

    setting up frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+      <h1>TravelTales</h1>
+      <p>Frontend Checking</p>
+    </div>
+  )
+}
+
+export default App
